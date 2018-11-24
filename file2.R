1864cabff6ae9612eed88e048d8f6a894a82f00a
KavyaDevarapalli <kavyad2019@email.iimcal.ac.in>
2018-11-24 20:39:34 +0530

    code downloaded from git

---
+++ file2.R
@@ -0,0 +1,41 @@
+#Creating vectors 
+#Data Structures in R
+c(2,4,6)
+?seq
+seq(1,10,1)
+seq(by=0.5,from=2,to=3)
+rep(1:3, times=5)
+?rep
+rep(c(3,6,7,2), times=4)
+(x2=c(1,2,13,4,5))
+(X3=c('a','abc'))
+class(x3)
+class(X3)
+(X3=letters[1:10])
+Letters[1:26]
+LETTERS[1:26]
+(X3=C('A','HENRY',4))
+(x5=c(3L,5L))
+(x6=0:100,3)
+(x6=seq(0:100,3))
+(x6=seq(0,100,3))
+(x7=seq(1,35,2))  
+plot(x7)  
+(x8=seq(1,length(x7),1))
+plot(length(x6))
+x6
+plot[length(x6)]
+x6[c(3)]
+x6[x6>30 &x6<45]
+x6[c(4,6,7)]
+x6[x6>20]
+x6[c(length(x6)-1)]  #remove last sescond 
+set.seed(1234)
+(x8=sample(1:50))
+(x9=sample(1:300))
+sort(x9)
+(x10=sort(x9,decreasing=T))
+(x10=(c(1,2)))
+x9
+x9[x9>30&x9<200]=999
+(x10=seq(1,5,length.out = 100000))
