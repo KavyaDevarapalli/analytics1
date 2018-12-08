7e8918cb84fbe16ddb5e8a06937eeb908c025ff7
KavyaDevarapalli <kavyad2019@email.iimcal.ac.in>
2018-12-08 19:30:02 +0530

    wrap up day 3

--- day3.R
+++ day3.R
@@ -28,4 +28,6 @@ arrange(sname, .by_marks=T,.by_gender=F)
 df1%>% select(sname, marks)%>% arrange(desc(marks))
 df1%>% sample_n(3)
 df1%>%sample_frac(.3)
-?frac
+??df1
+df1[sample(1:30,size=5);2]=NA
+
